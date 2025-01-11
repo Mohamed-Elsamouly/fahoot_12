7d2e65dbddebe928c6122b85e0e867ed44a4f1cf
Mohamed-Elsamouly <124587596+Mohamed-Elsamouly@users.noreply.github.com>
2025-01-11 09:18:13 +0200

    Add files via upload

---
+++ public/story_content/triggers.js
@@ -0,0 +1,27 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6XlBpEgDIYD":
+        Script1();
+        break;
+      case "5t9GxzrfaAY":
+        Script2();
+        break;
+      case "5gXClh0WXlY":
+        Script3();
+        break;
+      case "5br93Q7aJGC":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
